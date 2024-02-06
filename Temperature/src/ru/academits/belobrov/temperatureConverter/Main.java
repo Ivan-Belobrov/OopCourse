c77fb73c6a04d9484187175d3e22697e84189f52
Ivan-Belobrov <139527735+Ivan-Belobrov@users.noreply.github.com>
2024-02-06 16:22:18 +0700

    Add Java1

--- Temperature/src/ru/academits/belobrov/temperatureConverter/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.academits.belobrov.temperatureConverter;
-
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args){
-        SwingUtilities.invokeLater(TemperatureConverter::new);
-    }
-}
